48efa3e5983cdeae04e5dcd5e07f95073bdfc7a5
deCha <decha92@hotmail.com>
2021-06-19 16:35:04 -0500

    ran build and added redirects

---
+++ build/precache-manifest.c64abfe0141fe91168705fadd8c0757a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dd6edeb3e21c13994a9b3d6433d97c9a",
+    "url": "/index.html"
+  },
+  {
+    "revision": "8e03048bb96f496e8901",
+    "url": "/static/css/2.9233ae65.chunk.css"
+  },
+  {
+    "revision": "8e03048bb96f496e8901",
+    "url": "/static/js/2.5580e46f.chunk.js"
+  },
+  {
+    "revision": "d6b29286b0bbae6e17db84419d44fb6a",
+    "url": "/static/js/2.5580e46f.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2dc0b7cfbe745622d284",
+    "url": "/static/js/main.da165c44.chunk.js"
+  },
+  {
+    "revision": "f3f8fb5c825638ea9727",
+    "url": "/static/js/runtime-main.e93c7fde.js"
+  },
+  {
+    "revision": "5a413f609f00b4c3042c50939eb078e7",
+    "url": "/static/media/FullLogo.5a413f60.jpg"
+  }
+]);
